10326d2618e36705a562e749985caecae97dd5e1
Emariana Cohen <emariana.cohen@gmail.com>
2025-06-13 22:21:57 -0400

    Primer commit

---
+++ src/main/java/com/equipo/validador/App.java
@@ -0,0 +1,9 @@
+package com.equipo.validador;
+public class App {
+ public static void main(String[] args) {
+    String usuario = System.getenv("APP_USER");
+    if ("admin".equals(usuario)) {
+        System.out.println("¡Bienvenido administrador!");
+        }
+    }
+}
